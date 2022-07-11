24efe6f841c7822b174b81f6acf11aaadd1276ef
Eduard_Danilov <ed.danilov94@gmail.com>
2022-07-11 22:44:00 +0300

    dz 5

---
+++ c#/dz5/kolichestvoChetnihVMassive/Program.cs
@@ -0,0 +1,39 @@
+﻿int[] randomArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(100, 999);
+    }
+    return array;
+}
+
+void printArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int kolichestvoChetnihVMassive(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            count = count + 1;
+    }
+    return count;
+}
+
+
+Console.Write("input size: ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = randomArray(arraySize);
+
+printArray(myArray);
+
+Console.WriteLine("Количество четных чисел в массиве: " + kolichestvoChetnihVMassive(myArray));
