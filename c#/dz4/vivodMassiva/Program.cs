7753b857c931cb171b589ec667e58794d4db6443
Eduard_Danilov <ed.danilov94@gmail.com>
2022-07-11 22:06:46 +0300

    dz 4

---
+++ c#/dz4/vivodMassiva/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] randomArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+    return array;
+}
+
+
+void printArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+
+Console.Write("input count: ");
+int count = Convert.ToInt32(Console.ReadLine());
+Console.Write("input min: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("input max: ");
+int max = Convert.ToInt32(Console.ReadLine());
+int[] myArray = randomArray(count, min, max);
+printArray(myArray);
