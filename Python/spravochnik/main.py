898326ec07720c403f96b5ee045acb7ac138dc22
EduardDanilov <ed.danilov94@gmail.com>
2022-10-15 20:06:35 +0300

    dz10

--- Python/spravochnik/main.py
+++ Python/spravochnik/main.py
@@ -1,64 +1,43 @@
+
 import telebot
 import random
 from random import randint
 import config
 from telebot import types, TeleBot
-from distutils.cmd import Command
-import requests
-import json
+import csv
+
 
 bot = telebot.TeleBot(config.TOKEN)
 
-# @bot.message_handler(commands=['start'])
-# def send_start(message):
-# 	bot.send_message(message.chat.id, 'Приветствую тебя! Подробная информация в справке (/help) или в меню (/menu)')
 
+surname = ""
+name = ""
+phone = ""
+comment = ""
 
-contacts = {}
-# API_URL='https://7012.deeppavlov.ai/model'
+@bot.message_handler(commands=['help', 'start'])
+def help_message(message):
+    bot.send_message(message.chat.id, "Бот умеет делать:\n \
+/new_contact - новый контакт\n \
+/find_contact - найти контакт\n \
+/list - вывод списка всех контактов\n")
 
-contact = r'E:\Learning\GeekBrains\GIT\GB\Python\spravochnik\phone_directory.json'
-
-def save(contacts): #метод сохранения как void в C#
-    database = load()
-    database.update(contacts)
-    database = json.dumps(database)
-    with open(contact, "w") as fh: # "w" это метод write
-        json.dump(database, fh, indent=4, ensure_ascii=False)  # fh это псевдоним
-    print("Контакт сохранен")
-
-def load(): #метод сохранения как void в C#
-    with open(contact, "r", encoding="utf-8") as fh: # "w" это метод write
-        data=json.load(fh)
-    return data
-
-# API_TOKEN='5663390867:AAEYEU4JG3gc6bOoW9NsKGl-aLM_4Byy6iI'
-# bot=telebot.TeleBot(API_TOKEN)  #создали экземпляр класса телебот с уникальными данными
-
-# def start_message(message):
-#     bot.send_message(message.chat.id, 'Телефонный справочник')
-#     if contact not :
-#         load()
-#         bot.send_message(message.chat.id, "Справочник загружен")
-#     except:
-#         new_firstname()
 
 @bot.message_handler(commands=['new_contact'])
-#Фамилия, имя, номер, комментарий
-def new_firstname(message):
+def new_surname(message):
     msg = bot.send_message(message.chat.id, "Введи фамилию:")
-    bot.register_next_step_handler(msg, check_firstname)
-# Phone_Book["name"] = test.correct_name()
+    bot.register_next_step_handler(msg, check_surname)
 
-def check_firstname(message):
+def check_surname(message):
     chat_id = message.chat.id
     text = message.text
     if text.isalpha():
-        contacts["firstname"] = text
+        global surname
+        surname = text
         new_name(message)
     if not text.isalpha():
-        msg = bot.send_message(chat_id, "Введи фамилию")
-        bot.register_next_step_handler(msg, check_firstname)
+        msg = bot.send_message(chat_id, "Введи фамилию корректно")
+        bot.register_next_step_handler(msg, check_surname)
 
 def new_name(message):
     msg = bot.send_message(message.chat.id, "Введи имя:")
@@ -68,10 +47,11 @@ def check_name(message):
     chat_id = message.chat.id
     text = message.text
     if text.isalpha():
-        contacts["name"] = text
+        global name
+        name = text
         new_number(message)
     if not text.isalpha():
-        msg = bot.send_message(chat_id, "Введи имя")
+        msg = bot.send_message(chat_id, "Введи имя корректно")
         bot.register_next_step_handler(msg, check_name)
 
 def new_number(message):
@@ -82,8 +62,13 @@ def check_number(message):
     chat_id = message.chat.id
     text = message.text
     if text.isdigit():
-        contacts["number"] = text
-        new_comment(message)
+        if len(text) != 11:
+            msg = bot.send_message(chat_id, "Номер телефона должен состоять из 11 цифр в формате YYYYXXXXXXX")
+            bot.register_next_step_handler(msg, check_number)
+        else:
+            global phone
+            phone = text
+            new_comment(message)
     if not text.isdigit():
         msg = bot.send_message(chat_id, "Введи номер телефона из ЦИФР")
         bot.register_next_step_handler(msg, check_number)
@@ -95,49 +80,57 @@ def new_comment(message):
 def check_comment(message):
     chat_id = message.chat.id
     text = message.text
-    contacts["comment"] = text
-    save(contacts)
-    # new_name(message)
+    global comment
+    comment = text
+    add_contact()
+    bot.send_message(message.chat.id, "Контакт сохранен")
+
+def add_contact():
+    user_data = [[surname, name, phone, comment]]
+    with open("data.csv", "a", encoding="utf-8", newline="") as file:
+        writer = csv.writer(file, delimiter=",")
+        writer.writerows(user_data)
 
 
-# def initial_step_1(message):
-#     bot.send_message(message.chat.id, "Добро пожаловать в игру в конфеты")
-#     msg = bot.send_message(message.chat.id, "Для начала определись, сколько всего будет конфет. Введите число.")
-#     bot.register_next_step_handler(msg, check_candies)
+@bot.message_handler(commands=['list'])
+def all_contacts(message):
+    with open ("data.csv", newline="", encoding="utf-8") as file:
+        reader = csv.DictReader(file, delimiter=',')
+        i = 1
+        for row in reader:
+            contact_surname = row['surname']
+            contact_name = row['name']
+            contact_phone = row['phone']
+            contact_comment = row['comment']
+            bot.send_message(message.chat.id, f"Контакт №{i}:\n \
+                                                Фамилия: {contact_surname}\n\
+                                                Имя: {contact_name}\n \
+                                                Номер телефона: {contact_phone}\n \
+                                                Комментарий: {contact_comment}\n")
+            i+=1
 
-# def initial_step_2(message):
-#     msg = bot.send_message(message.chat.id, "Сейчас назови количество конфет, которое можно брать за ход. Введите число.")
-#     bot.register_next_step_handler(msg, check_bot_can_take)
+@bot.message_handler(commands=['find_contact'])
+def find_contact(message):
+    msg = bot.send_message(message.chat.id, "Введи параметры поиска (фамилию / имя / номер телефона / комментарий):")
+    bot.register_next_step_handler(msg, search)
 
-# def check_candies(message) -> str:
-#     chat_id = message.chat.id
-#     text = message.text
-#     if text.isdigit() and text.isdigit() > 0:
-#         candy_logic.total_candies = int(text)
-#         bot.send_message(message.chat.id, f'Доступно {candy_logic.total_candies} конфет')
-#         initial_step_2(message)
-#     if not text.isdigit():
-#         msg = bot.send_message(chat_id, "Введи ЦИФРАМИ (положительными, строго больше 0) количество конфет!")
-#         bot.register_next_step_handler(msg, check_candies)
-
-
-@bot.message_handler(commands=['all'])
-def show_all(message):
-    bot.send_message(message.chat.id, "Вот список фильмов")
-    bot.send_message(message.chat.id, ", ".append(contacts))
-
-# @bot.message_handler(commands=['wiki'])
-# # def wiki(message):
-# #     quest = message.text.split()[1:] #приняли запрос от пользователя и положили в переменную сплит и в массив
-# #     qq=" ".append(quest)
-# #     data = {'question_raw': [qq]} #формируем запрос
-# #     try:  #если нашёл ответ на запрос
-# #         res = requests.post(API_URL, json=data, verify=False).json() # посылаем запрос и  получаем обратоно
-# #         bot.send_message(message.chat.id, res)
-# #     except:   #если НЕ нашёл ответ на запрос
-# #         bot.send_message(message.chat.id, "что-то  я ничего не нашёл :((")
-# #     # bot.send_message(message.chat.id, ", ".append(contacts))
-
+def search(message):
+    search_parameter = message.text
+    with open ("data.csv", newline="", encoding="utf-8") as file:
+        reader = csv.DictReader(file, delimiter=',')
+        i = 1
+        for row in reader:
+            if search_parameter in row:
+                contact_surname = row['surname']
+                contact_name = row['name']
+                contact_phone = row['phone']
+                contact_comment = row['comment']
+                bot.send_message(message.chat.id, f"Контакт №{i}:\n \
+                                                    Фамилия: {contact_surname}\n\
+                                                    Имя: {contact_name}\n \
+                                                    Номер телефона: {contact_phone}\n \
+                                                    Комментарий: {contact_comment}\n")
+            i+=1
 
 
 
