3c3b4972fbefd0156605792aff4de66ca77cc697
EduardDanilov <ed.danilov94@gmail.com>
2023-07-25 23:48:54 +0300

    javascript homework 4

--- JavaScript/hw4/script.js
+++ JavaScript/hw4/script.js
@@ -27,12 +27,14 @@
 
 
 
-// Используя Math.random() вам необходимо генерировать цифры от 0 до 9, и создать массив состоящий из 5 таких элементов
+// Используя Math.random() вам необходимо генерировать цифры от 0 до 9, и создать массив, состоящий из 5 таких элементов
 // 1. Рассчитать сумму элементов этого массива
 // 2. Найти минимальное число
 // 3. Найти есть ли в этом массиве число 3
 
 // const arr = [];
+// let sumArrayElements = 0;
+// let minimalArrayNumber = 9;
 
 // const getRandomArbitrary = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
@@ -40,7 +42,15 @@
 //     arr.push(getRandomArbitrary(0, 9));
 // }
 
+// for (let i = 0; i < arr.length; i++) {
+//   (arr[i] < minimalArrayNumber) ? minimalArrayNumber = arr[i] : minimalArrayNumber = minimalArrayNumber;
+//   sumArrayElements += arr[i];
+// }
+
 // console.log(arr);
+
+// console.log(`Минимальное число в массиве - ${minimalArrayNumber}`);
+// console.log(`Сумма элементов массива ${sumArrayElements}`);
 
 // (arr.includes(3)) ? console.log(`Цифра 3 найдена под индексом ${arr.indexOf(3)}`) : console.log('Цифра 3 не найдена');
 
