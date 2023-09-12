b6f5556551e33483c793ec696897bf28b452cf9a
EduardDanilov <ed.danilov94@gmail.com>
2023-09-12 20:00:30 +0300

    add front js hw5

---
+++ JavaScript/frontJS/hw5/script.js
@@ -0,0 +1,36 @@
+const catalogItems = JSON.parse(catalogData)
+
+const catalog = document.querySelector('.catalog');
+
+catalogItems.forEach(({id, name, description, price, image, alt, link}) => {
+    const productBox = document.createElement('div');
+    catalog.appendChild(productBox);
+    productBox.classList.add('product')
+
+    const productImage = document.createElement('img');
+    productBox.appendChild(productImage);
+    productImage.classList.add('product__img')
+    productImage.alt = alt;
+    productImage.src = image;
+
+    const productName = document.createElement('a');
+    productBox.appendChild(productName);
+    productName.classList.add('product__name')
+    productName.href = link;
+    productName.textContent = name;
+
+    const productDescription = document.createElement('p');
+    productBox.appendChild(productDescription);
+    productDescription.textContent = description;
+    productDescription.classList.add('product__description');
+
+    const productPrice = document.createElement('p');
+    productBox.appendChild(productPrice);
+    productPrice.classList.add('product__price');
+    productPrice.textContent = `${price} руб.`;
+    
+    const productId = document.createElement('p');
+    productBox.appendChild(productId);
+    productId.classList.add('product__id');
+    productId.textContent = `Код при заказе: ${id}`;
+});
