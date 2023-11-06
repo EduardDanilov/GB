2ee27e7b45812873c0ecee2bb2bf73287d12c91a
EduardDanilov <ed.danilov94@gmail.com>
2023-11-06 19:21:19 +0300

    add hw3

---
+++ JavaScript/Vue2/hw3/src/components/ProductDetails.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <ProductList v-for="product in productList" :key="product.id" :product="product"/>
+    </div>
+</template>
+
+<script>
+import ProductList from "./ProductList.vue";
+
+export default {
+    name: "ProductDetails",
+    components: {
+        ProductList
+    },
+    data () {
+    return {
+      productList: [
+        {
+          id: 0,
+          name: 'milk',
+          price: 10,
+          isAvailable: true
+        },
+        {
+          id: 1,
+          name: 'sugar',
+          price: 20,
+          isAvailable: false
+        },
+        {
+          id: 2,
+          name: 'phone',
+          price: 50,
+          isAvailable: true
+        },
+        {
+          id: 3,
+          name: 'car',
+          price: 500000,
+          isAvailable: true
+        }
+      ]
+    }
+  },
+};
+</script>
---
+++ JavaScript/Vue2/hw3/src/components/ProductList.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="prodlist">
+        <div class="name">{{ product.name }}</div>
+        <div class="price">{{ product.price }}</div>
+        <div class="isavailable">{{ product.isAvailable ? 'Available' : 'Out of stock' }}</div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'ProductList',
+  props: {
+    product: {
+      type: Object,
+    }
+  },
+  methods: {
+    sortUp () {
+      this.productList = this.productList.sort((a, b) =>
+        a.price > b.price ? 1 : -1)
+    },
+    sortDown () {
+      this.productList = this.productList.sort((a, b) =>
+        a.price < b.price ? 1 : -1)
+    }
+  }
+}
+</script>
+<style>
+.prodlist {
+    display: flex;
+    gap: 20px;
+    margin-bottom: 15px;
+}
+</style>
