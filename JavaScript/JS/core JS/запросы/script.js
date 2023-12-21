388280a8f73610c9efab5971489afe65476a452e
EduardDanilov <ed.danilov94@gmail.com>
2023-12-21 21:41:44 +0300

    fix gubs

---
+++ JavaScript/JS/core JS/запросы/script.js
@@ -0,0 +1,12 @@
+// (async () => {
+//     const url = "https://catfact.ninja/fact";
+//     const response = await fetch(url);
+//     console.log(response.status);
+//     console.log(response.ok);
+//     const fact = await response.json().then(response => response.fact)
+//     console.log(fact);
+// })();
+
+// fetch("https://catfact.ninja/fact")
+//     .then((response) => response.json())
+//     .then((obj) => outer.textContent = obj.fact);
